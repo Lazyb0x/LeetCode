8721bb8570340d9b0aa78f163e930e2b4c594bc9
Lazyb0x <gym1998421@gmail.com>
2019-07-25 10:32:02 +0800

    modified Q025

--- src/leetcode/Q025.java
+++ src/leetcode/Q025.java
@@ -1,14 +1,10 @@
 package leetcode;
 
-//import java.util.LinkedList;
-
 class Solution25 {
     public ListNode reverseKGroup(ListNode head, int k) {
         ListNode curr = new ListNode(0);
         curr.next = head;
         head = curr;
-        
-//        LinkedList<ListNode> stack = new LinkedList<ListNode>();
         
         jump:
         while (curr!=null) {
@@ -16,7 +12,6 @@ class Solution25 {
         	ListNode tmp = curr.next;
         	while (c>0) {
         		if (tmp!=null) {
-//        			stack.push(tmp);
         			tmp = tmp.next;
         			c--;
         		}
@@ -26,7 +21,6 @@ class Solution25 {
         	}
         	
         	c=k;
-
         	ListNode left = curr.next;
         	ListNode right;
         	while (c>0) {
@@ -40,29 +34,8 @@ class Solution25 {
         	ListNode begin = curr.next;
         	curr.next = tmp;
         	curr = begin;
-        	
-//        	while (!stack.isEmpty()) {
-//        		ListNode node = stack.pop();
-//        		curr.next = node;
-//        		curr = node;
-//        	}
-//        	curr.next = tmp;
         }
         return head.next;
-    }
-    
-    public boolean isReverse(ListNode node, int k) {
-    	int c = k;
-    	while (c>0) {
-    		if (node!=null) {
-    			node = node.next;
-    			c--;
-    		}
-    		else {
-    			return false;
-    		}
-    	}
-    	return true;
     }
 }
 
@@ -71,7 +44,7 @@ public class Q025 {
 		Solution25 s = new Solution25();
 		int[] a = {1,2,3,4,5};
 		ListNode l = ListNodeManage.toListNode(a);
-		ListNode l2 = s.reverseKGroup(l, 4);
+		ListNode l2 = s.reverseKGroup(l, 2);
 		System.out.println(ListNodeManage.toString(l2));
 	}
 }
